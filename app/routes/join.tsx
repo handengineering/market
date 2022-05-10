2beddfafc5817de432d79553409d392e3ca53512
Lewis Flude <lewis@lewisflude.com>
2022-05-10 10:01:31 +0100

    Remove unused import in routes/join.tsx

--- app/routes/join.tsx
+++ app/routes/join.tsx
@@ -3,7 +3,7 @@ import type {
   LoaderFunction,
   MetaFunction,
 } from "@remix-run/node";
-import { json, redirect } from "@remix-run/node";
+import { json } from "@remix-run/node";
 import { Form, Link, useActionData, useSearchParams } from "@remix-run/react";
 import * as React from "react";
 
