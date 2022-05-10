c52b1e39d35d86d549785fdbce2624c376b7f99f
Lewis Flude <lewis@lewisflude.com>
2022-05-10 09:35:27 +0100

    Remove loader from root.tsx

--- app/root.tsx
+++ app/root.tsx
@@ -1,6 +1,4 @@
 import type {
-  LinksFunction,
-  LoaderFunction,
   MetaFunction,
 } from "@remix-run/node";
 import { json } from "@remix-run/node";
@@ -13,23 +11,12 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 
-import { getUser } from "./session.server";
-
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
   title: "Hand Engineering Market",
   viewport: "width=device-width,initial-scale=1",
 });
 
-type LoaderData = {
-  user: Awaited<ReturnType<typeof getUser>>;
-};
-
-export const loader: LoaderFunction = async ({ request }) => {
-  return json<LoaderData>({
-    user: await getUser(request),
-  });
-};
 
 export default function App() {
   return (
