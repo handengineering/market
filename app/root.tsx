7fbdd6644762b318634faf3497425df682586f3e
Lewis Flude <lewis@lewisflude.com>
2022-05-11 09:17:50 +0100

    Add ErrorBoundaryComponent to root.tsx

--- app/root.tsx
+++ app/root.tsx
@@ -1,6 +1,4 @@
-import type {
-  MetaFunction,
-} from "@remix-run/node";
+import { ErrorBoundaryComponent, MetaFunction } from "@remix-run/node";
 import {
   Links,
   LiveReload,
@@ -17,11 +15,20 @@ export const meta: MetaFunction = () => ({
   viewport: "width=device-width,initial-scale=1",
 });
 
+export const ErrorBoundary: ErrorBoundaryComponent = ({ error }) => {
+  return (
+    <div>
+      <h1>Error</h1>
+      <p>{error.message}</p>
+      <p>The stack trace is:</p>
+      <pre>{error.stack}</pre>
+    </div>
+  );
+};
 
 export default function App() {
-
   globalStyles();
-  
+
   return (
     <html lang="en">
       <head>
