a70f77bb2c21d313fc8fca7c6c68ee74c8496659
Lewis Flude <lewis@lewisflude.com>
2022-05-12 18:07:07 +0100

    List raffles in dashboard.tsx

--- app/routes/dashboard.tsx
+++ app/routes/dashboard.tsx
@@ -9,21 +9,32 @@ import DiscordStatusTextFields from "~/components/DiscordStatusTextFields";
 import DiscordStatusWrapper from "~/components/DiscordStatusWrapper";
 import Main from "~/components/Main";
 import Sidebar from "~/components/Sidebar";
-import { DiscordGuildMember, getDiscordProfileByUserId } from "~/models/discordProfile.server";
+import {
+  DiscordGuildMember,
+  getDiscordProfileByUserId,
+} from "~/models/discordProfile.server";
 import { User } from "~/models/user.server";
 import { authenticator } from "~/services/auth.server";
+import { getRaffles, Raffle } from "~/models/raffle.server";
+import Card from "~/components/Card";
 
 const guildId = "605444240016801879";
 
 type LoaderData = {
   user: User;
+  raffles: Raffle[];
   discordProfile: DiscordProfile | null;
   result: DiscordGuildMember | null;
 };
 
 export default function Screen() {
-  const { user, discordProfile, result } = useLoaderData() as LoaderData;
-  const hasJoinedDiscord = discordProfile && result && result.user && result.user.id === discordProfile.id;
+  const { user, raffles, discordProfile, result } =
+    useLoaderData() as LoaderData;
+  const hasJoinedDiscord =
+    discordProfile &&
+    result &&
+    result.user &&
+    result.user.id === discordProfile.id;
 
   return (
     <AppContainer>
@@ -49,37 +60,53 @@ export default function Screen() {
         )}
       </Sidebar>
       <Main>
-        <DiscordStatusWrapper>
-          {discordProfile && discordProfile.displayAvatarUrl && (
-            <DiscordAvatar
-              src={`https://cdn.discordapp.com/avatars/${discordProfile.id}/${discordProfile.displayAvatarUrl}`}
-            />
+        <Card>
+          <h2>Discord Status</h2>
+
+          <DiscordStatusWrapper>
+            {discordProfile && discordProfile.displayAvatarUrl && (
+              <DiscordAvatar
+                src={`https://cdn.discordapp.com/avatars/${discordProfile.id}/${discordProfile.displayAvatarUrl}`}
+              />
+            )}
+            <DiscordStatusTextFields>
+              <span>
+                {discordProfile && discordProfile.displayName
+                  ? `Signed in as ${discordProfile.displayName}`
+                  : "Not signed into Discord"}
+              </span>
+
+              <span>
+                You {hasJoinedDiscord ? "are" : "are not"} a member of our
+                Discord
+              </span>
+            </DiscordStatusTextFields>
+          </DiscordStatusWrapper>
+          {!hasJoinedDiscord ? (
+            <Form method="post" action="/auth/discord">
+              <Button color="primary">Connect Discord Profile</Button>
+            </Form>
+          ) : (
+            <Form
+              method="post"
+              action={`/discordProfile/${discordProfile.id}/delete`}
+            >
+              <Button color="danger">Remove Discord Profile</Button>
+            </Form>
           )}
-          <DiscordStatusTextFields>
-            <span>
-              {discordProfile && discordProfile.displayName
-                ? `Signed in as ${discordProfile.displayName}`
-                : "Not signed into Discord"}
-            </span>
-
-            <span>
-              You {hasJoinedDiscord ? "are" : "are not"} a member of our Discord
-            </span>
-          </DiscordStatusTextFields>
-        </DiscordStatusWrapper>
-
-        {!hasJoinedDiscord ? (
-          <Form method="post" action="/auth/discord">
-            <Button color="primary">Connect Discord Profile</Button>
-          </Form>
-        ) : (
-          <Form
-            method="post"
-            action={`/discordProfile/${discordProfile.id}/delete`}
-          >
-            <Button color="danger">Remove Discord Profile</Button>
-          </Form>
-        )}
+        </Card>
+        <Card>
+          <h2>Raffles</h2>
+          <ul>
+            {raffles.map((raffle) => {
+              return (
+                <li>
+                  {raffle.name} {raffle.id}
+                </li>
+              );
+            })}
+          </ul>
+        </Card>
       </Main>
     </AppContainer>
   );
@@ -99,14 +126,19 @@ export let loader: LoaderFunction = async ({ request }) => {
     Authorization: `Bot ${discordBotToken}`,
   };
 
-  let discordGuildMember = discordProfile && await fetch(
-    `https://discordapp.com/api/guilds/${guildId}/members/${discordProfile.id}`,
-    {
-      headers: authHeaders,
-    }
-  );
+  let discordGuildMember =
+    discordProfile &&
+    (await fetch(
+      `https://discordapp.com/api/guilds/${guildId}/members/${discordProfile.id}`,
+      {
+        headers: authHeaders,
+      }
+    ));
 
-  const result: DiscordGuildMember | null = discordGuildMember && await discordGuildMember.json();
+  const result: DiscordGuildMember | null =
+    discordGuildMember && (await discordGuildMember.json());
 
-  return { user, discordProfile, result };
+  const raffles: Raffle[] = await getRaffles();
+
+  return { user, raffles, discordProfile, result };
 };
