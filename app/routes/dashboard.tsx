e3cc3f4c18d2258d562822856154581f5fc0ae91
Lewis Flude <lewis@lewisflude.com>
2022-05-17 21:12:07 +0100

    Add log out to raffles page

--- app/routes/dashboard.tsx
+++ app/routes/dashboard.tsx
@@ -6,7 +6,6 @@ import DiscordAvatar from "~/components/DiscordAvatar";
 import DiscordStatusTextFields from "~/components/DiscordStatusTextFields";
 import DiscordStatusWrapper from "~/components/DiscordStatusWrapper";
 import Main from "~/components/Main";
-import Sidebar from "~/components/Sidebar";
 import { getDiscordProfileByUserId } from "~/models/discordProfile.server";
 import { authenticator } from "~/services/auth.server";
 import Card from "~/components/Card";
@@ -17,6 +16,7 @@ import type {
   DiscordProfile,
 } from "~/models/discordProfile.server";
 import type { Raffle } from "~/models/raffle.server";
+import Navigation from "~/components/Navigation";
 
 const guildId = "605444240016801879";
 
@@ -36,66 +36,69 @@ export default function Screen() {
     result.user.id === discordProfile.id;
 
   return (
-    <AppContainer>
-      <Sidebar>
-        <h1>Hand Engineering Market</h1>
+    <>
+      <Navigation>
+        <Link to="/raffles">
+          <Button>All Raffles</Button>
+        </Link>
+        <Link to="/dashboard">
+          <Button>Dashboard</Button>
+        </Link>
         {user ? (
-          <>
-            <p>Signed in as {user.email}</p>
-
-            <Link to="/logout">
-              <Button color="danger">Log Out</Button>
-            </Link>
-          </>
+          <Link to="/logout">
+            <Button color="danger">Log Out ({user.email})</Button>
+          </Link>
         ) : (
-          <div>
+          <>
             <Link to="/join">
               <Button color="primary">Sign up</Button>
             </Link>
             <Link to="/login">
               <Button>Log In</Button>
             </Link>
-          </div>
+          </>
         )}
-      </Sidebar>
-      <Main>
-        <Card>
-          <h2>Discord Status</h2>
+      </Navigation>
+      <AppContainer>
+        <Main>
+          <Card>
+            <h2>Discord Status</h2>
 
-          <DiscordStatusWrapper>
-            {discordProfile && discordProfile.displayAvatarUrl && (
-              <DiscordAvatar
-                src={`https://cdn.discordapp.com/avatars/${discordProfile.id}/${discordProfile.displayAvatarUrl}`}
-              />
+            <DiscordStatusWrapper>
+              {discordProfile && discordProfile.displayAvatarUrl && (
+                <DiscordAvatar
+                  src={`https://cdn.discordapp.com/avatars/${discordProfile.id}/${discordProfile.displayAvatarUrl}`}
+                />
+              )}
+              <DiscordStatusTextFields>
+                <span>
+                  {discordProfile && discordProfile.displayName
+                    ? `Signed in as ${discordProfile.displayName}`
+                    : "Not signed into Discord"}
+                </span>
+
+                <span>
+                  You {hasJoinedDiscord ? "are" : "are not"} a member of our
+                  Discord
+                </span>
+              </DiscordStatusTextFields>
+            </DiscordStatusWrapper>
+            {!hasJoinedDiscord ? (
+              <Form method="post" action="/auth/discord">
+                <Button color="primary">Connect Discord Profile</Button>
+              </Form>
+            ) : (
+              <Form
+                method="post"
+                action={`/discordProfile/${discordProfile.id}/delete`}
+              >
+                <Button color="danger">Remove Discord Profile</Button>
+              </Form>
             )}
-            <DiscordStatusTextFields>
-              <span>
-                {discordProfile && discordProfile.displayName
-                  ? `Signed in as ${discordProfile.displayName}`
-                  : "Not signed into Discord"}
-              </span>
-
-              <span>
-                You {hasJoinedDiscord ? "are" : "are not"} a member of our
-                Discord
-              </span>
-            </DiscordStatusTextFields>
-          </DiscordStatusWrapper>
-          {!hasJoinedDiscord ? (
-            <Form method="post" action="/auth/discord">
-              <Button color="primary">Connect Discord Profile</Button>
-            </Form>
-          ) : (
-            <Form
-              method="post"
-              action={`/discordProfile/${discordProfile.id}/delete`}
-            >
-              <Button color="danger">Remove Discord Profile</Button>
-            </Form>
-          )}
-        </Card>
-      </Main>
-    </AppContainer>
+          </Card>
+        </Main>
+      </AppContainer>
+    </>
   );
 }
 
--- app/routes/raffles.tsx
+++ app/routes/raffles.tsx
@@ -1,10 +1,19 @@
-import { Link, Outlet } from "@remix-run/react";
+import type { User } from "@prisma/client";
+import { Link, Outlet, useLoaderData } from "@remix-run/react";
+import type { LoaderFunction } from "@remix-run/server-runtime";
 import AppContainer from "~/components/AppContainer";
 import Button from "~/components/Button";
 import Main from "~/components/Main";
 import Navigation from "~/components/Navigation";
+import { authenticator } from "~/services/auth.server";
+
+type LoaderData = {
+  user: User;
+};
 
 export default function Raffles() {
+  const { user } = useLoaderData() as LoaderData;
+
   return (
     <>
       <Navigation>
@@ -14,6 +23,20 @@ export default function Raffles() {
         <Link to="/dashboard">
           <Button>Dashboard</Button>
         </Link>
+        {user ? (
+          <Link to="/logout">
+            <Button color="danger">Log Out ({user.email})</Button>
+          </Link>
+        ) : (
+          <>
+            <Link to="/join">
+              <Button color="primary">Sign up</Button>
+            </Link>
+            <Link to="/login">
+              <Button>Log In</Button>
+            </Link>
+          </>
+        )}
       </Navigation>
       <AppContainer>
         <Main>
@@ -23,3 +46,11 @@ export default function Raffles() {
     </>
   );
 }
+
+export let loader: LoaderFunction = async ({ request }) => {
+  let user = await authenticator.isAuthenticated(request, {
+    failureRedirect: "/login",
+  });
+
+  return { user };
+};
