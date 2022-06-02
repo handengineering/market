d1d2febfcc42103bfc9a5f9fef454d69e9ba1807
Lewis Flude <lewis@lewisflude.com>
2022-06-02 11:02:16 +0100

    Convert FlexContainer to use tailwind

--- app/components/FlexContainer/index.tsx
+++ app/components/FlexContainer/index.tsx
@@ -1,22 +1,7 @@
-import { styled } from "~/styles/stitches.config";
+export interface FlexContainerProps {
+  children: React.ReactNode;
+}
 
-const FlexContainer = styled("div", {
-  display: "flex",
-  flexWrap: "wrap",
-  flexDirection: "row",
-  gap: "$5",
-  variants: {
-    layout: {
-      desktop: {
-        flexDirection: "row",
-        alignItems: "initial",
-      },
-      mobile: {
-        flexDirection: "column",
-        alignItems: "center",
-      },
-    },
-  },
-});
-
-export default FlexContainer;
+export default function FlexContainer({ children }: FlexContainerProps) {
+  <div className="flex flex-row flex-wrap gap-6">{children}</div>;
+}
