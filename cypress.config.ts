092546335695268b3601f56fd8d85e6baa59419c
Lewis Flude <lewis@lewisflude.com>
2022-08-01 16:26:08 +0100

    Set dynamic baseUrl

--- cypress.config.ts
+++ cypress.config.ts
@@ -5,6 +5,6 @@ export default defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    baseUrl: "http://localhost:8080",
+    baseUrl: `http://localhost:${process.env.PORT}`,
   },
 });
