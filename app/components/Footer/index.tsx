00cb65840b3d8d0b78f21dce4e1271c35dc62d01
Lewis Flude <lewis@lewisflude.com>
2022-10-15 12:52:13 +0100

    Add credits section

--- app/components/Footer/index.tsx
+++ app/components/Footer/index.tsx
@@ -1,8 +1,15 @@
+import { Link } from "@remix-run/react";
+
 export default function Footer(): React.ReactElement {
   return (
     <footer className="mt-16 flex w-full max-w-7xl flex-col items-center justify-between gap-4 py-8 px-8 md:flex-row">
       <div className="text-neutral-500">
-        Copyright © 2022 Have A Nice Day Engineering Ltd. All rights reserved.
+        <span className="mr-4">
+          Copyright © 2022 Have A Nice Day Engineering Ltd. All rights reserved.{" "}
+        </span>
+        <Link className="text-neutral-200" to="/credits">
+          Credits
+        </Link>
       </div>
     </footer>
   );
---
+++ app/routes/credits.tsx
@@ -0,0 +1,22 @@
+export default function Index() {
+  return (
+    <p>
+      Thank you to{" "}
+      <a
+        href="https://www.instagram.com/koenromers/"
+        title="Koen Romers Instagram"
+      >
+        Koen Romers
+      </a>{" "}
+      and
+      <a
+        href="https://www.instagram.com/moarcoffeeplzzz/"
+        title="Ryan Lester Instagram"
+      >
+        {" "}
+        Ryan Lester
+      </a>{" "}
+      for images used throughout the Hand Engineering store
+    </p>
+  );
+}
